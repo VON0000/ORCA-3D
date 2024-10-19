ff8aaaefa304f38a50101b6a2d11c68d859bf956
Lu ZHANG <zhanglu.930@outlook.com>
2024-10-19 22:17:54 +0800

    feat: plot two level of flights

--- plot.ml
+++ plot.ml
@@ -34,21 +34,17 @@ let output_obstacle =
 (* 利用gnuplot绘图 *)
 let plot_to_screen time =
   Printf.fprintf outc "set terminal x11\n";
-  (* 使用 x11 作为终端 *)
   Printf.fprintf outc "set xrange [-%d:%d]\n" dscreen dscreen;
   Printf.fprintf outc "set yrange [-%d:%d]\n" dscreen dscreen;
 
-  (* 使用 replot 以确保图形能更新 *)
+  (* Plot points based on levels with different colors *)
   Printf.fprintf outc
-    "plot './results/obstacle' not w l lw 3, './results/points' not pt 7 ps \
-     2, './results/speeds' w l, './results/newspeeds' w l, './results/routes' \
-     lc 4 w l, './results/boites' w l, './results/memory' not w l lc 3\n";
+    "plot './results/obstacle' not w l lw 3, './results/points_level_0' not pt \
+     7 ps 2 lc 'green', './results/points_level_1' not pt 7 ps 2 lc 'red', \
+     './results/speeds' w l, './results/newspeeds' w l, './results/routes' lc \
+     4 w l, './results/boites' w l, './results/memory' not w l lc 3\n";
 
   flush outc;
-
-  (* 确保命令发送 *)
-
-  (* 添加短暂延时，确保图形有时间渲染 *)
   Unix.sleep 1;
   ()
 
@@ -63,25 +59,37 @@ let output acfts boites time =
   close_out fichobstacle;
 
   let coeff = 50. (*zoom d'affichage des rayons des vitesse*) in
-  let fichp = open_out "./results/points" in
+  let fichp0 = open_out "./results/points_level_0" in
+  let fichp1 = open_out "./results/points_level_1" in
   let fichb = open_out "./results/boites" in
   let fichs = open_out "./results/speeds" in
   let fichns = open_out "./results/newspeeds" in
   for i = 0 to Const.dim - 1 do
     let local_acft = List.nth acfts i in
     if local_acft.active then (
-      Printf.fprintf fichp "%f %f\n" local_acft.position.x local_acft.position.y;
-      flush fichp;
+      (* Separate points based on level *)
+      if local_acft.level = 0 then
+        Printf.fprintf fichp0 "%f %f\n" local_acft.position.x
+          local_acft.position.y
+      else
+        Printf.fprintf fichp1 "%f %f\n" local_acft.position.x
+          local_acft.position.y;
+
+      flush fichp0;
+      flush fichp1;
+
       Printf.fprintf fichs "%f %f\n%f %f\n\n" local_acft.position.x
         local_acft.position.y
         (local_acft.position.x +. (coeff *. local_acft.speedopt.x))
         (local_acft.position.y +. (coeff *. local_acft.speedopt.y));
       flush fichs;
+
       Printf.fprintf fichns "%f %f\n%f %f\n\n" local_acft.position.x
         local_acft.position.y
         (local_acft.position.x +. (coeff *. local_acft.speed.x))
         (local_acft.position.y +. (coeff *. local_acft.speed.y));
       flush fichns;
+
       List.iter
         (fun { x; y } ->
           Printf.fprintf fichb "%f %f\n"
@@ -91,7 +99,8 @@ let output acfts boites time =
       Printf.fprintf fichb "\n";
       flush fichb)
   done;
-  close_out fichp;
+  close_out fichp0;
+  close_out fichp1;
   close_out fichb;
   close_out fichs;
   close_out fichns;
